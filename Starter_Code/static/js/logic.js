0379ef63736fd7bcea31b4028cfed6404dc45657
katherinewinder1 <112666732+katherinewinder1@users.noreply.github.com>
2023-04-01 14:04:04 -0700

    updating logic.js

--- Starter_Code/static/js/logic.js
+++ Starter_Code/static/js/logic.js
@@ -12,28 +12,45 @@ var myMap = L.map("map", {
 // 
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
 // Create a circle for each earthquake
-    var earthquakes = []
-    data.features.forEach(i => {
-        var lat = i.geometry.coordinates[1];
-        var lng = i.geometry.coordinates[0];
+    function styleInfo(feature) {
+        //var earthquakes = []
+        //data.features.forEach(i => {
+            //var lat = i.geometry.coordinates[1];
+            //var lng = i.geometry.coordinates[0];
 
-        earthquakes.push(
-            L.circle([lat,lng], {
-                stroke: false,
-                fillOpacity: 0.5,
-                fillColor: circleColor(i.geometry.coordinates[2]),
-                radius: circleSize(i.properties.mag)
-            }).bindPopup("<h3>" + i.properties.place + "</h3><hr><p>" +
-                new Date(i.properties.time) + "</p", {
-                    maxWidth: 500
-        })
-    )
-});
+            //earthquakes.push(
+                //L.circle([lat,lng], {
+                    return {
+                    stroke: false,
+                    fillOpacity: 0.5,
+                    fillColor: circleColor(feature.geometry.coordinates[2]),
+                        radius: circleSize(feature.properties.mag)
+                //}).bindPopup("<h3>" + i.properties.place + "</h3><hr><p>" +
+                    //new Date(i.properties.time) + "</p", {
+                        //maxWidth: 500
+                    }
+                };
+    //)
+//});
 
 // Determine circle size
-function circleSize(mag) {
-    return mag * 30000;
+function circleSize(magnitude) {
+    if (magnitude === 0) {
+        return 1;
+    }
+
+    return magnitude * 30000;
 }
+
+L.geoJson(data, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng);
+    },
+    style: styleInfo, onEachFeature: function (feature, layer) {
+        layer.bindPopup("Mag: " + feature.properties.mag + "<br>depth: " + feature.geometry.coordinates[2] + "<br>Loc: " + feature.properties.place
+        );
+    }
+}).addTo(map);
 
 function circleColor(depth) {
     var color = "#FFEDA0";
@@ -54,10 +71,10 @@ var legend = L.control({
     position: "bottomright"
 });
 
-legend.onAdd = function(myMap) {
-    var div = L.DomUtil.create("div", "legend");
-    labels = ['<strong>Magnitude and Depth</strong>']
-    categories = ['-10 to 10', '10 to 30', '30 to 50', '50 to 70', '70 to 90', '90+'];
+legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend");
+    var labels = ['<strong>Magnitude and Depth</strong>']
+    var categories = ['-10 to 10', '10 to 30', '30 to 50', '50 to 70', '70 to 90', '90+'];
     for (var i = 0; i < categories.length; i++) {
         div.innerHTML += 
         labels.push('<i class="circle" style="background:' + circleColor(categories[i])
