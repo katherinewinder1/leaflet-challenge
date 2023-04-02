c4a1e3b91828740423aad4f9aad3fac0593172b6
katherinewinder1 <112666732+katherinewinder1@users.noreply.github.com>
2023-04-02 14:20:16 -0700

    adding final code and readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # leaflet-challenge
+
+In this project I referenced the United States Geological Survey (USGS) for earthquake data over the last 7 days. I used the URL from the JSON representation provided by USGS to visualize the data on a map using Leaflet. I included popups showing the magnitude, depth, and location of each earthquake and a legend representing the depths.
+
--- Starter_Code/static/js/logic.js
+++ Starter_Code/static/js/logic.js
@@ -13,25 +13,15 @@ var myMap = L.map("map", {
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
 // Create a circle for each earthquake
     function styleInfo(feature) {
-        //var earthquakes = []
-        //data.features.forEach(i => {
-            //var lat = i.geometry.coordinates[1];
-            //var lng = i.geometry.coordinates[0];
-
-            //earthquakes.push(
-                //L.circle([lat,lng], {
-                    return {
-                    stroke: false,
-                    fillOpacity: 0.5,
-                    fillColor: circleColor(feature.geometry.coordinates[2]),
+        
+        return {
+            stroke: false,
+            fillOpacity: 0.5,
+            fillColor: circleColor(feature.geometry.coordinates[2]),
                         radius: circleSize(feature.properties.mag)
-                //}).bindPopup("<h3>" + i.properties.place + "</h3><hr><p>" +
-                    //new Date(i.properties.time) + "</p", {
-                        //maxWidth: 500
-                    }
-                };
-    //)
-//});
+                
+                }
+            };
 
 // Determine circle size
 function circleSize(magnitude) {
@@ -56,22 +46,22 @@ function circleColor(depth) {
     
     switch(true) {
         case depth < 10:
-            return "#FFEDA0";
+            return "#FFFFE0";
         
         case depth < 30: 
-            return "#FEB24C";
+            return "#FFB6C1";
         
         case depth < 50: 
-            return"#FD8D3C";
+            return "#FF69B4";
         
         case depth < 70:
-            return "#E31A1C";
+            return "#FF1493";
 
         case depth < 90:
-            return "#98ee00";
+            return "#9370DB";
 
         case depth > 90:
-            return "#FF0000"
+            return "#36013F"
         
     }
 };
@@ -83,25 +73,11 @@ var legend = L.control({
 
 legend.onAdd = function() {
     var div = L.DomUtil.create("div", "info legend");
-    var labels = ['<strong>Magnitude and Depth</strong>'];
-    //var categories = ['-10 to 10', '10 to 30', '30 to 50', '50 to 70', '70 to 90', '90+'];
     var categories = [-10, 10, 30, 50, 70, 90];
-    var colors = ["#FFEDA0", "#FEB24C", "#FD8D3C", "#E31A1C", "#98ee00", "#FF0000"]
+    var colors = ["#FFFFE0", "#FFB6C1", "#FF69B4", "#FF1493", "#9370DB", "#36013F"]
     for (var i = 0; i < categories.length; i++) {
     div.innerHTML += "<i style=background:" + colors[i] + "></i>" + categories[i] + 
-        (categories[i + 1] ? "&ndash;" + categories[i+1] + "<br>" : "+");
-    
-        //"<h2>Earthquake Depth</h2>",
-        //"<p class='l10'>-10 to 10</p>",
-        //"<p class='l30'>10 to 30</p>",
-        //"<p class='l50'>30 to 50</p>",
-        //"<p class='l70'>50 to 70</p>",
-        //"<p class='l90'>70 to 90</p>",
-        //"<p class='g90'>90+</p>",
-    //].join("");
-        //labels.push('<i class="circle" style="background:' + circleColor(categories[i])
-        //+ (categories[i] ? categories[i] : '+'));
-    //} div.innerHTML = labels.join("<br>");
+        (categories[i + 1] ? "&ndash;" + categories[i+1] + "<br>" : "+");  
     }
     return div;
 };
